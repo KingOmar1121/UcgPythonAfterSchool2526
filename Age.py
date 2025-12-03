7ef1c9a0bedc65a7924622e381b49319a922e3e4
KingOmar1121 <jonquaviousomar@gmail.com>
2025-12-02 18:24:14 -0600

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,11 @@
+#Program descriptiopn
+
+
+age_years = int(input("Enter your age in years"))
+
+months = age_years * 12
+days = age_years * 365 
+print("_____________________________-")
+print("Your are", months, "months old")
+print("Thats about", days, "days!")
+print("______________________________")
